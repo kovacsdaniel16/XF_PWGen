7ee0bd698265661478de5b5088340e792e138717
kovacsdaniel16 <kovacsdanieel@gmail.com>
2021-10-25 16:38:26 +0200

    2 lap létrehozva, PW lapon két gomb létrehozva

---
+++ XF_PWGen/PWGenPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF_PWGen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PWGenPage : ContentPage
+    {
+        public PWGenPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btGenerate_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
